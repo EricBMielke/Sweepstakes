6f92f110249a576c65e4f21a2816014339f61070
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-07-23 14:47:39 -0500

    Adding the customer class with the required properties

---
+++ Sweepstakes/Contestant.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Contestant
+    {
+        private string firstName;
+        private string lastName;
+        private int registrationNumber;
+        private string emailAddress;
+
+        public string FirstName
+        {
+            get
+            {
+                return firstName;
+            }
+            set
+            {
+                firstName = value;
+            }
+        }
+        public string LastName
+        {
+            get
+            {
+                return lastName;
+            }
+            set
+            {
+                lastName = value;
+            }
+        }
+        public int RegistrationNumber
+        {
+            get
+            {
+                return registrationNumber;
+            }
+            set
+            {
+                registrationNumber = value;
+            }
+        }
+        public string EmailAddress
+        {
+            get
+            {
+                return emailAddress;
+            }
+            set
+            {
+                emailAddress = value;
+            }
+        }
+
+
+
+    }
+}
