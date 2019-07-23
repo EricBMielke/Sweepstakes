bb603f3278260b00440020146a946849e8d10aed
EricBMielke <51930708+EricBMielke@users.noreply.github.com>
2019-07-23 15:06:44 -0500

    Adding the required classes and interfaces, along with the methods that are necessary for the aforementioned classes/interfaces

---
+++ Sweepstakes/Sweepstakes.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweepstakes
+{
+    class Sweepstakes
+    {
+        public string Sweepstake(string Name)
+        {
+            return "foo";
+        }
+        public void RegisterContestant(Contestant contestant)
+        {
+
+        }
+        public string PickWinner()
+        {
+            return "foo";
+        }
+        public void PrintContestantInfo(Contestant contestant)
+        {
+
+        }
+
+    }
+    public interface UserInformation
+    {
+
+    }
+    interface ISweepstakesManager
+    {
+
+    }
+}
